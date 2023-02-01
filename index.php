c6295bb1ce3b36caaae4e3128e21619e7ea2c914
Iuliia Shikhanova <yshihanova@gmail.com>
2023-02-01 16:17:21 +0000

    move{footer}: move footer down

--- index.php
+++ index.php
@@ -8,9 +8,16 @@ include_once './src/templates/navbar.php';
 // Actual view
 include_once './src/templates/index.php';
 
+
+
+
+
 $url = "https://bitpay.com/api/rates";
 $json = json_decode(file_get_contents($url));
 $dollar = $btc = 0;
 foreach($json as $obj){
   echo '1 bitcoin = $'. $obj->rate .' '. $obj->name .' ('. $obj->code .')<br>';
 }
+
+// Footer
+include_once './src/templates/footer.html';
--- src/templates/head.php
+++ src/templates/head.php
@@ -6,6 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="./src/js/navbar.js"></script>
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              colors: {
+                yellow: '#F9ED69', 
+                orange: '#F08A5D',
+                maroon: '#B83B5E',
+                purple: '#6A2C70E',
+              }
+            }
+          }
+        }
+      </script>
     <title>Document</title>
 </head>
 
