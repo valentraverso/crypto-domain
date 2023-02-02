e9af794c3006a9ade26423b4ae53f2ac88c0804d
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-02-02 11:30:58 +0100

    Changed paths of index-logged

--- src/templates/head.php
+++ src/templates/head.php
@@ -1,7 +1,7 @@
 <?php
 /* Main Path of your project 👇 */
 
-define ('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/assembler/develop-your-project-in-php/');
+define ('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/develop-your-project-in-php');
 ?>
 
 <head>
@@ -9,7 +9,7 @@ define ('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/assembler/develop-your-pro
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script defer src=".<?php echo BASE_URL ;?>/src/js/navbar.js"></script>
+    <script defer src="<?php echo BASE_URL ;?>/src/js/navbar.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
     <script>
         tailwind.config = {
--- src/templates/navbarLoggedUser.php
+++ src/templates/navbarLoggedUser.php
@@ -1,6 +1,6 @@
 <nav class="relative px-4 py-4 flex justify-between items-center bg-white">
 		<a class="text-3xl font-bold leading-none text-maroon" href="#">
-            <img width="50px"src="../img/logo.png">
+            <img width="50px"src="<?php echo BASE_URL ;?>/img/logo.png">
 		</a>
 		<div class="lg:hidden">
 			<button class="navbar-burger flex items-center text-maroon p-3">
@@ -20,7 +20,7 @@
 		<div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
       		<ul class="flex flex-col p-4 mt-4 rounded">
 				<li class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-yellow rounded-full transition duration-200">
-					<button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"><img src="../img/avatarUser.png" width="30px"> <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
+					<button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"><img src="<?php echo BASE_URL ;?>/img/avatarUser.png" width="30px"> <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
 					<!-- Dropdown menu -->
 					<div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
 						<ul class="py-2 text-sm text-gray-700 bg-yellow transition duration-200" aria-labelledby="dropdownLargeButton">
@@ -45,7 +45,7 @@
 		<nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
 			<div class="flex items-center mb-8">
 				<a class="mr-auto text-3xl font-bold leading-none" href="#">
-                    <img width="50px"src="../img/logo.png">		
+                    <img width="50px"src="<?php echo BASE_URL ;?>/img/logo.png">		
 				</a>
 				<button class="navbar-close">
 					<svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-maroon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -63,6 +63,9 @@
 					</li>
 					<li class="mb-1">
 						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-yellow hover:text-maroon rounded" href="#">Sell</a>
+					</li>
+					<li class="mb-1">
+						<a class="block p-4 text-sm font-semibold text-gray-400 hover:bg-yellow hover:text-maroon rounded" href="#">Dashboard</a>
 					</li>
 				</ul>
 			</div>
