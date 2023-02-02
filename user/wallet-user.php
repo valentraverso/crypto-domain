8c58f4b07e0b02624caed4e9e91523f8acfeba5a
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-02-02 17:57:07 +0100

    tryconnect mysqlphp

---
+++ user/wallet-user.php
@@ -0,0 +1,15 @@
+<?php
+// Head
+$pageTitle = 'Wallet';
+include_once '../src/templates/head.php';
+
+// Navbar
+include_once '../src/templates/navbarLoggedUser.php';
+
+// Actual view
+include_once '../src/templates/walletUser.php';
+
+// Footer
+include_once '../src/templates/footer.html';
+
+?>
