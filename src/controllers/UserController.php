02798f4538bcbef42bfbfcff0e9a150778d55373
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-02-03 10:50:31 +0100

    Updated DB finish Create User

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,15 +1,16 @@
 <?php
 
-require_once '../controllers/DbConnection.php'; 
+require_once '../controllers/DbConnection.php';
 
 class Users extends Connection{
+
     // Create
     public function createUser($idWallet, $email, $password, $firstName, $lastName, $birthDate, $favCoin, $status, $role){
          
-        $createQuery = $this->con->prepare("INSERT INTO `users` 
+        $createQuery = $this->con->prepare("INSERT INTO users 
         (`id_user`, `id_wallet`, `email`, `password`, `first_name`, `last_name`, `birth_date`, `fav_coin`, `avatar`, `status`, `role`) 
         VALUES 
-        ('NULL', ':idWallet', ':email', ':password', ':firstName', ':lastName', ':birthDate', ':favCoin', 'NULL', ':status', ':role')");
+        (NULL, :idWallet, :email, :password, :firstName, :lastName, :birthDate, :favCoin, '', :status, :role)");
         $createQuery->bindParam(':idWallet', $idWallet);
         $createQuery->bindParam(':email', $email);
         $createQuery->bindParam(':password', $password);
@@ -20,8 +21,9 @@ class Users extends Connection{
         $createQuery->bindParam(':status', $status);
         $createQuery->bindParam(':role', $role);
         $createQuery->execute();
-    }
-    
+    } 
+
+    // Read
     public function readUserData($queryExtend){
 
         $sqlQuery = $this->con->prepare("SELECT * FROM users $queryExtend");
