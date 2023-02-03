16f5fe9200a921ee51703f0a23e74b94a8f12ab5
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-02-03 12:32:44 +0100

    Feat {Login}::Added login feature

---
+++ src/funcs/logInUser.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('../controllers/UserController.php');
+
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+
+$user = new Users();
+$userMatched = $user->readUserData("WHERE email='$email' AND password='$password'");
+
+if ($userMatched){
+    session_start();
+    $_SESSION['email'] = $email;
+    header('Location: ../../index.php');
+}else{
+    header('Location: ../../user/login.php?msg=error');
+}
+
+?>
