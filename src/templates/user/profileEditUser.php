4f48174d0bcbc225e83b0485108cdc5cfaab343f
Valentino Traverso <vt20032003@live.com.ar>
2023-02-06 15:55:27 +0100

    styles to btn change-profile.php

--- src/templates/user/profileEditUser.php
+++ src/templates/user/profileEditUser.php
@@ -93,7 +93,7 @@ $userData = $user->readUserData("WHERE id_user='$idUser'");
 </a>
         
         <button
-          class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+          class="hover:shadow-form rounded-md bg-[#6A64F1] ml-5 py-3 px-7 text-base font-semibold text-white outline-none">
           Submit
         </button>
         
