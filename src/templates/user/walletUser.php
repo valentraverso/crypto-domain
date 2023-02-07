102f62b74a4c35d033fec2dae6e2b43d2287d2f2
Valentino Traverso <vt20032003@live.com.ar>
2023-02-07 17:40:05 +0100

    add admin folder

--- src/templates/user/walletUser.php
+++ src/templates/user/walletUser.php
@@ -1,8 +1,6 @@
 <?php
-include BASE_PATH.'/src/templates/components/head.php';
-include BASE_PATH.'/src/templates/components/navbarLoggedUser.php';
+include_once BASE_PATH.'/src/templates/components/head.php';
+include_once BASE_PATH.'/src/templates/components/navbarLoggedUser.php';
 include_once BASE_PATH.'/src/templates/components/dynamicChart.php';
 include_once BASE_PATH.'/src/templates/components/footer.html';
-
-
 ?>
--- walletextra.php
+++ walletextra.php
@@ -6,4 +6,4 @@ $coinQuery = $coin->getWallet("WHERE id_user = ".$_SESSION['id_user']);
 $monedas = $coinQuery['json_coins'];
 $idWallet = $coinQuery['id_wallet'];
 
-$coin->updateWallet($monedas, '$.EUR', 0, 11);
+$coin->updateWallet($monedas, '$.EUR', $monedas['EUR'] + 10, 11);
