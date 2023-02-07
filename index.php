9a3ff504cc40f83357b51988e2cffa71c2b121cd
Valentino Traverso <vt20032003@live.com.ar>
2023-02-07 10:36:25 +0100

    feat{add: user to wallet table}

--- index.php
+++ index.php
@@ -10,13 +10,3 @@ if(isset($_SESSION['id_user'])){
 } else {
     include_once BASE_PATH.'/src/templates/index.php';
 }
-
-require_once BASE_PATH.'/src/controllers/WalletController.php';
-
-$coin = new Wallet();
-
-$coinQuery = $coin->getWallet("WHERE id_user = ".$_SESSION['id_user']);
-$monedas = $coinQuery['json_coins'];
-$idWallet = $coinQuery['id_wallet'];
-
-$coin->updateWallet($monedas, '$.EUR', 100, 11);
