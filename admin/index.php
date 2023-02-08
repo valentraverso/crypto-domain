3fe50758fb42035f2a8f966078941b33bc8575ea
Valentino Traverso <vt20032003@live.com.ar>
2023-02-08 10:50:11 +0100

    list users in admin page

--- admin/index.php
+++ admin/index.php
@@ -29,7 +29,7 @@ if(isset($admin)){
 
     $admin = new Admin();
 
-    print_r($admin->getAllUsers());
+    $users = $admin->getAllUsers();
 
     ?>
 <div class="align-baseline shadow-md rounded-lg">
@@ -37,23 +37,23 @@ if(isset($admin)){
             <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Coin
+                        ID
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Amount
+                            Email
                             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
                         </div>
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Cotization
+                            Name
                             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
                         </div>
                     </th>
                     <th scope="col" class="px-6 py-3">
                         <div class="flex items-center">
-                            Price
+                            Birth
                             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
                         </div>
                     </th>
@@ -66,31 +66,39 @@ if(isset($admin)){
                 </tr>
             </thead>
             <tbody>
+                <?php
+
+                foreach($users as $user){
+                    ?>
+                    
                 <tr class="bg-white border-b">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-                        BTC
+                        <?php echo $user['id_user']; ?>
                     </th>
                     <td class="px-6 py-4">
-                        10
+                    <?php echo $user['email']; ?>
                     </td>
                     <td class="px-6 py-4">
-                        Sliver
+                    <?php echo $user['first_name'] . $user['last_name']; ?>
                     </td>
                     <td class="px-6 py-4">
-                        $2999
+                    <?php echo $user['birth_date']; ?>
                     </td>
                     <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Buy</a>
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">See Wallet</a>
                     </td>
                     <td class="px-6 py-4 text-right">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Sell</a>
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Deactivate</a>
                     </td>
                 </tr>
+                <?php
+                }
+                ?>
             </tbody>
             <tfoot>
                 <tr class="font-semibold text-gray-900">
-                    <th scope="row" class="px-6 py-3 text-base">Total</th>
-                    <td class="px-6 py-3">21,000</td>
+                    <th scope="row" class="px-6 py-3 text-base">Total Users:</th>
+                    <td class="px-6 py-3"><?php echo sizeof($user); ?></td>
                 </tr>
             </tfoot>
         </table>
