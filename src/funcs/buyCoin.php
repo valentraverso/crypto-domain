8b895c14915bcd16da7f9462426cbb8c1c00f2d4
Iuliia Shikhanova <yshihanova@gmail.com>
2023-02-08 14:22:32 +0000

    update{database}: buy coins and update json with new data

--- src/funcs/buyCoin.php
+++ src/funcs/buyCoin.php
@@ -3,7 +3,6 @@
 require_once "../controllers/pathControllers.php";
 require_once BASE_PATH . "/src/controllers/CoinsController.php";
 require_once BASE_PATH . "/src/controllers/WalletController.php";
-// require_once BASE_PATH . "/src/controllers/DbConnection.php";
 require_once BASE_PATH . "/src/apiCoin.php";
 
 /**
@@ -15,27 +14,71 @@ require_once BASE_PATH . "/src/apiCoin.php";
 
 session_start();
 $walletRec = $_SESSION["id_user"];
-//$wallet_id = $_SESSION["id_wallet"]; //<==
-//$json = $_SESSION["json_coins"]; <==
-
 
 if (isset($_POST['quantity-money']) && isset($_POST['select-coin'])) {
     $quantity_money = $_POST['quantity-money'];
     $selected_coin = $_POST['select-coin'];
   
     $displayCoin = new Coins();
-
     $price = $displayCoin->setCoin($selected_coin, "EUR")["RAW"][$selected_coin]["EUR"]["PRICE"];
-    $quantity_coin = $quantity_money/$price;
+    $quantity_coin = bcdiv($quantity_money/$price,"1",5);
 
-    $transaction = new Coin();
-    $transaction->createTransaction("0", $walletRec , "1", $quantity_coin, $price);
+$wallet = new Wallet();
 
-    $walletRegist = new Wallet();
-    $walletRegist->createWallet($walletRec);
+$walletUser = $wallet->getWallet("WHERE id_user=$walletRec"); // usuario
+$coinObjUser = $walletUser['json_coins'];
+$idWalletUser = $walletUser['id_wallet'];
+$actualWallet = (json_decode($coinObjUser, true)["EUR"]); // EUR en wallet
+
+$walletAdmin = $wallet->getWallet("WHERE id_user=0"); // admin
+$coinObjAdmin = $walletAdmin['json_coins'];
+$idWalletAdmin = $walletUser['id_wallet'];
+$eurMaster = (json_decode($coinObjAdmin,true)[$selected_coin]); //coins en wallet admin
 
 
-  header("Location: ../../user/wallet-user.php"); 
-  } 
+if($actualWallet >= $quantity_money && $eurMaster >= $quantity_coin){
+    echo "Buy it!";
+
+  $selected_coin = $_POST['select-coin'];
+    echo $selected_coin;
+    print_r($actualWallet);
+    echo "<hr>" . $quantity_money;
+    echo $walletRec; 
+
+  $transaction = new Coin();
+  $transaction->createTransaction("0", $walletRec , "1", $quantity_coin, $price); 
+
+  $wallet->updateWallet($coinObjUser, '$.EUR', $actualWallet - $quantity_money, $walletRec); //user: cambia cantidad de eur
+
+
+  $walletUser = $wallet->getWallet("WHERE id_user=$walletRec"); // usuario
+  $coinObjUser = $walletUser['json_coins'];
+  $idWalletUser = $walletUser['id_wallet'];
+  $walletCoins = (json_decode($coinObjUser,true)[$selected_coin]);
+
+  $wallet->updateWallet($coinObjUser, '$.'.$selected_coin.'', $walletCoins+$quantity_coin, $walletRec); //user: cambia cantidad de coin
+
+
+
+  $walletAdmin = $wallet->getWallet("WHERE id_user=0"); 
+  $coinObjAdmin = $walletAdmin['json_coins'];
+  $idWalletAdmin = $walletUser['id_wallet'];
+  $eurMaster = (json_decode($coinObjAdmin, true)["EUR"]); 
+
+  $wallet->updateWallet($coinObjAdmin, '$.EUR', $eurMaster + $quantity_money, 0); //admin: cambia cantidad de eur
+
   
+
+  $walletAdmin = $wallet->getWallet("WHERE id_user=0"); 
+  $coinObjAdmin = $walletAdmin['json_coins'];
+  $idWalletAdmin = $walletUser['id_wallet'];
+  $coinMaster = (json_decode($coinObjAdmin,true)[$selected_coin]);
+
+  $wallet->updateWallet($coinObjAdmin, '$.'.$selected_coin.'', $coinMaster-$quantity_coin, 0); //admin: cambia cantidad de coin
+
+   header("Location: ../../user/wallet-user.php");  
+}else{
+  echo "No money no honey";
+}
+} 
 ?>
