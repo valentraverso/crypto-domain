13d09d7e64683d044a959ce6f571d811fe3ed3c4
Valentino Traverso <vt20032003@live.com.ar>
2023-02-08 10:09:34 +0100

    fix templates and views

--- user/wallet-user.php
+++ user/wallet-user.php
@@ -1,8 +1,18 @@
 <?php
-// Head
+require_once '../src/controllers/pathControllers.php';
+
 $pageTitle = 'Wallet';
 
-require_once '../src/controllers/pathControllers.php';
+session_start();
+
+$id = $_SESSION['id_user'];
+
+require_once BASE_PATH.'/src/controllers/WalletController.php';
+
+$wallet = new Wallet;
+
+$coin = $wallet->getWallet("WHERE id_user=$id");
+$coinObj = json_decode($coin['json_coins']);
 
 include BASE_PATH.'/src/templates/user/walletUser.php';
 ?>
