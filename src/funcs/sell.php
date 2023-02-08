afc279b8c1ea5477f0b956a379b8df1a8d44e8f0
Albert Gomis <albert.gomis.20@gmail.com>
2023-02-08 15:20:43 +0100

    feat: sell

--- src/funcs/sell.php
+++ src/funcs/sell.php
@@ -2,10 +2,11 @@
 require_once('../controllers/pathControllers.php');
 require_once BASE_PATH . "/src/apiCoin.php";
 require_once(BASE_PATH.'/src/controllers/transactionsController.php');
+require_once(BASE_PATH.'/src/controllers/WalletController.php');
 
 session_start();
-
 $idUser = $_SESSION["id_user"];
+
 $coin = $_GET["coin"];
 $amount = $_GET["amount"];
 
@@ -14,13 +15,63 @@ $price = $showCoin->setCoin($coin, "EUR")["RAW"][$coin]["EUR"]["PRICE"];
 
 $amountCoin = bcdiv($amount/$price,"1",5);
 
-$walletRec = 0;
+$wallet = new Wallet();
 
-$walletSend = $_SESSION["id_user"];
+$walletUser = $wallet->getWallet("WHERE id_user=$idUser");
+$coinObjUser = ($walletUser['json_coins']);
+$coinUser= (json_decode($coinObjUser,true)[$coin]);
+
+$walletUser = $wallet->getWallet("WHERE id_user=$idUser");
+$coinObjUser = ($walletUser['json_coins']);
+$eurUser= (json_decode($coinObjUser,true)["EUR"]);
+
+$walletMaster = $wallet->getWallet("WHERE id_user=0");
+$coinObjMaster = ($walletMaster['json_coins']);
+$eurMaster = (json_decode($coinObjMaster,true)["EUR"]);
+
+$walletMaster = $wallet->getWallet("WHERE id_user=0");
+$coinObjMaster = ($walletMaster['json_coins']);
+$coinMaster = (json_decode($coinObjMaster,true)[$coin]);
+
+// echo $coinMaster . "<br>";
+// echo $eurMaster . "EurMaster <br>";
+// echo $eurUser . "<br>";
+
+if($eurMaster - $amount >= 0 && $coinUser - $amountCoin >= 0 ){
+    echo "You can sell";
+    $walletRec = 0;
+    
+    $walletSend = $_SESSION["id_user"];
+    
+    
+    
+    // $transaction = new Transaction();
+    // $transaction -> createTransaction($walletSend, $walletRec, $coin, $amountCoin, $price);
+    
+    
+    $wallet->updateWallet($coinObjMaster, '$.EUR',$eurMaster - $amount,0);
+    
+    $walletMaster = $wallet->getWallet("WHERE id_user=0");
+    $coinObjMaster = ($walletMaster['json_coins']);
+    $coinMaster = (json_decode($coinObjMaster,true)[$coin]);
+    $eurMaster = (json_decode($coinObjMaster,true)["EUR"]);
+    
+    $wallet->updateWallet($coinObjMaster, '$.'.$coin.'',$coinMaster + $amountCoin, 0);
+    
+    
+    $wallet->updateWallet($coinObjUser, '$.'.$coin.'',$coinUser - $amountCoin, $idUser);
+    $walletUser = $wallet->getWallet("WHERE id_user=$idUser");
+    $coinObjUser = ($walletUser['json_coins']);
+    $eurUser= (json_decode($coinObjUser,true)["EUR"]);
+
+    $wallet->updateWallet($coinObjUser, '$.EUR',$eurUser + $amount,$idUser);
+    
+    // header("Location: ../../user/wallet-user.php");
+} else{
+    echo "You can not sell";
+}
 
 
 
-$transaction = new Transaction();
-$transaction -> createTransaction($walletSend, $walletRec, $coin, $amountCoin, $price);
 
-header("Location: ../../user/wallet-user.php");
+
