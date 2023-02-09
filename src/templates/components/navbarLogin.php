bb81b6b304cba4991c25047ea61edff546ce6d71
Ruben Zafra Traver <rubenreamps@gmail.com>
2023-02-09 09:44:15 +0100

    Admin Page Style start

--- src/templates/components/navbarLogin.php
+++ src/templates/components/navbarLogin.php
@@ -1,6 +1,6 @@
-<nav class="relative pt-4 flex flex-col justify-center items-center bg-white text-maroon text-3xl font-bold">
-    <p>Crypto Domain</p>
+<nav class="relative pt-4 flex flex-col justify-center items-center bg-white text-maroon text-3xl font-bold mb-12">
+    <h1 class="text-4xl font-bold mt-0 mb-6 drop-shadow-xl text-transparent bg-clip-text bg-maroon">Crypto Domain</h1>
 		<a class="text-3xl font-bold leading-none flex space-between" href="<?php echo BASE_URL;?>">
       <img width="50px" src="<?php echo BASE_URL;?>/img/logo.png">
-		</a>		
+		</a>
 </nav>
